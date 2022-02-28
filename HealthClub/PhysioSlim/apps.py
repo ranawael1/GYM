42165247149e83908b29591bafe8e38421cea2d5
rana <ranawael9721@gmail.com>
2022-02-28 18:49:02 +0200

    changing project name

---
+++ HealthClub/PhysioSlim/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PhysioslimConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'PhysioSlim'
---
+++ HealthClub/PhysioSlim/serializers.py
@@ -0,0 +1,3 @@
+import imp
+from rest_framework import serializers
+from .models import *
