6432d879050b2630997632870d4cf358b82c5826
sohila-mo <sohilamhmmd@gmail.com>
2022-02-28 14:12:57 -0800

    login form

--- HealthClub/PhysioSlim/serializers.py
+++ HealthClub/PhysioSlim/serializers.py
@@ -1,3 +1,10 @@
 import imp
 from rest_framework import serializers
+from django.contrib.auth.models import User 
 from .models import *
+
+
+class Usererializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
--- HealthClub/PhysioSlim/views.py
+++ HealthClub/PhysioSlim/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.models import Group, User #User model to access users and admins
+from .serializers import Usererializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+@api_view(['GET'])
+def all(request):
+    all= User.objects.all()
+    st = Usererializer(all, many=True)
+    return Response(st.data)
+
