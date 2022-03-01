5edc8baa6eb8249cf6bed2cfbd2ef02c0db93602
shady bassily <shadybassily@yahoo.com>
2022-03-01 16:31:38 +0200

    done

--- HealthClub/PhysioSlim/serializers.py
+++ HealthClub/PhysioSlim/serializers.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
 from .models import User,branch,Offer
-from django.db import models
-from django.contrib.auth.hashers import make_password
+from rest_framework.validators import UniqueValidator
+
 
 class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+            required=True,
+            validators=[UniqueValidator(queryset=User.objects.all())]
+            )
     class Meta:
         model = User
         fields = ('id','username', 'email','password', 'age', 'gender', 'phone',)
