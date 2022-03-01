62e8525f6bb90181224a8be8b15d1bb3ba558c9d
sohybnaim <sohya50@gmail.com>
2022-03-01 12:14:12 +0200

    create branch urls

--- HealthClub/PhysioSlim/urls.py
+++ HealthClub/PhysioSlim/urls.py
@@ -9,6 +9,12 @@ urlpatterns = [
     path('users/', views.users, name='users'),
     path('user/<user_id>', views.user, name='user'),
     #path('add-user/', views.add_user, name='add-user'),
-    path('verify/', views.verify_code, name='verify-code'),  
+    path('verify/', views.verify_code, name='verify-code'),
+    #api-branch  
+    path('api-all', views.api_all_branch, name='api-all'),
+    path('api-one/<br_id>', views.api_one_branch, name='api-one'),
+    path('api-add', views.api_add_branch, name='api-add'),
+    path('api-edit/<br_id>', views.api_edit_branch, name='api-edit'),
+    path('api-del/<br_id>', views.api_del_branch, name='api-del'),
 ]
 
