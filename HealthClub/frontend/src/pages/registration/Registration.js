0e7cac2414b40c412f8be0bf38c5ce2d84cf1309
rana <ranawael9721@gmail.com>
2022-03-02 16:08:49 +0200

    create react app

---
+++ HealthClub/frontend/src/pages/registration/Registration.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Registration(props) {
+    return (
+        <div>
+            <h1>Registration</h1>
+        </div>
+    );
+}
+
+export default Registration;
