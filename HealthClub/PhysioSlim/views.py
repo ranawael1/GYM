0c07c5d56497db0c1eb80b8d04e4d4195bae3ab2
shady bassily <shadybassily@yahoo.com>
2022-03-09 16:09:24 +0200

    accepting changes

--- HealthClub/PhysioSlim/views.py
+++ HealthClub/PhysioSlim/views.py
@@ -260,6 +260,7 @@ def subscribeToClass(request, class_id):
 #display favorite classes
 @login_required(login_url='login')
 def favoriteClasses(request):
+    notifications = UserNotifications(request)
     all_subscribers = ClassSubscribers.objects.filter(subscriber=request.user).values_list('favclass_id', flat=True)
     #getting the classes the user subscribed to
     favorite_classes = ClassSubscribers.objects.filter(subscriber_id=request.user.id).values_list('favclass_id', flat=True)
@@ -267,7 +268,7 @@ def favoriteClasses(request):
     fav_classes=list(favorite_classes)
     #getting the info of the favorite classes from the Class Model
     classes = Class.objects.filter(id__in = fav_classes)
-    context={'classes':classes, 'all_subscribers':all_subscribers}
+    context={'classes':classes, 'all_subscribers':all_subscribers,'notifications':notifications}
 
     return render(request, 'physio-slim/favorites.html', context)
 
